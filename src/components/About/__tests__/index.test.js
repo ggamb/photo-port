6e878174962664d47d7624ef740fb3bf470e4d29
ggamb <glenngambardella372@gmail.com>
2021-12-11 09:59:55 -0500

    added Nav tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-const { asFragment } = render(<About />);
+
 
 afterEach(cleanup);
 
 describe('About component', () => {
-    //renders About test
+    //renders About baseline test
     it('renders', () => {
         render(<About />);
     });
 
     it('matches snapshot DOM node structure', () => {
-        // render About
+        const { asFragment } = render(<About />);
+        // render About snapshot test
         expect(asFragment()).toMatchSnapshot();
     });
 })
