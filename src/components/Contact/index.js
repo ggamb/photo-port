22e3382726dd7b5782c5e6477ac46cf7f32cfdff
ggamb <glenngambardella372@gmail.com>
2021-12-13 10:31:23 -0500

    added handleChange and handleSubmit to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
 
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
     return (
         <section>
@@ -9,17 +19,17 @@ function ContactForm() {
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" onSubmit={handleSubmit}>Submit</button>
             </form>
         </section>
     )
